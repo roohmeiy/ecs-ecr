7f3a2fa8243695df0bed2cc0f09debd60b678998
Payal Sharma <roohmeiy@gmail.com>
2025-07-22 10:34:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+// Middleware to serve static files (if needed)
+app.use(express.static('public'));
+
+// Route that displays the message
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World , This is my App</h1>');
+});
+
+// Start the server on 0.0.0.0
+app.listen(PORT, HOST, () => {
+    console.log("Hey you are champion");
+    console.log("Server is running on http://${HOST}:${PORT}`");
+    console.log("Also accessible via http://localhost:${PORT}");
+});
